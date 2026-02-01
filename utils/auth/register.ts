d31665f11afc5ecc2831a37e0a1831909751f6b7
chiagger <busca.chiara.cb@gmail.com>
2026-02-01 23:23:19 +0100

    remove console logs

--- utils/auth/register.ts
+++ utils/auth/register.ts
@@ -17,9 +17,7 @@ export const handleRegisterEmail = async (
     password: string,
     setErrorMessage: (message: string) => void,
 ) => {
-    console.log("enters")
     if (name && surname && email && password) {
-        console.log("valid fields", name, surname, email, password)
         try {
             const usersCollection = collection(db, "users");
             const q = query(usersCollection, where("email", "==", `${email}`));
