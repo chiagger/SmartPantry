d2fde565ca0fdb18482b2dc0b07682fa1b1d4d68
chiagger <busca.chiara.cb@gmail.com>
2026-02-02 23:27:24 +0100

    feat: display image

--- app/(protected)/(footer)/home.tsx
+++ app/(protected)/(footer)/home.tsx
@@ -1,6 +1,8 @@
 import { ThemedText } from "@/components/ThemedText";
 import colors from "@/constants/Colors";
-import { View } from "react-native";
+import { Image, View } from "react-native";
+
+const banana = require("../../../assets/images/icons/banana.png");
 
 export default function Home() {
   const theme = "dark"; // or "dark", depending on your theme logic
@@ -18,6 +20,7 @@ export default function Home() {
       <ThemedText style={{ fontSize: 28, fontFamily: "Montserrat-SemiBold" }}>
         Home
       </ThemedText>
+      <Image source={banana} style={{ width: 64, height: 64, marginTop: 12 }} />
     </View>
   );
 }
